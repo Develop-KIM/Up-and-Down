3406074797ccb7bbdfde7998c18e799b457453f0
Donghwan Kim <kimdonghwan913@gmail.com>
2023-10-30 11:48:24 +0900

    final commit

--- js/main.js
+++ js/main.js
@@ -1,62 +1,62 @@
 let Number = 0;
-let PlayBtn = document.getElementById("submit_btn");
-let UserInput = document.getElementById("user_input");
-let ResultArea = document.getElementById("result_area");
-let ResetBtn = document.getElementById("reset_btn");
+let playBtn = document.getElementById("submit_btn");
+let userInput = document.getElementById("user_input");
+let resultArea = document.getElementById("result_area");
+let resetBtn = document.getElementById("reset_btn");
 let Chances = 10;
-let GameOver = false;
-let ChanceArea = document. getElementById("chance_area");
+let gameOver = false;
+let chanceArea = document. getElementById("chance_area");
 let History = [];
 
-ResetBtn.addEventListener("click", reset);
-PlayBtn.addEventListener("click", play);
-UserInput.addEventListener("focus",function(){UserInput.value = ""});
+resetBtn.addEventListener("click", reset);
+playBtn.addEventListener("click", play);
+userInput.addEventListener("focus",function(){userInput.value = ""});
 
-function RandomNumber() {
+function randomNumber() {
   Number = Math.floor(Math.random() * 100)+1;
   console.log(Number)
 }
 
 function play() {
-  let UserValue = UserInput.value;
-  if(UserValue<1 || UserValue>100) {
-    ResultArea.textContent = "1~100사이로 입력해줘 우딩"
+  let userValue = userInput.value;
+  if(userValue<1 || userValue>100) {
+    resultArea.textContent = "1~100사이로 입력해줘 우딩"
     return;
   }
-  if(History.includes(UserValue)) {
-    ResultArea.textContent = "정신차려 이미 입력했어 우딩"
+  if(History.includes(userValue)) {
+    resultArea.textContent = "정신차려 이미 입력했어 우딩"
     return;
   }
   Chances --;
-  ChanceArea.textContent = `남은 기회: ${Chances}`;
-  if(UserValue>Number) {
-    ResultArea.textContent = "돼지야 내려!";
-  } else if(UserValue<Number) {
-    ResultArea.textContent = "돼지야 올려!";
+  chanceArea.textContent = `남은 기회: ${Chances}`;
+  if(userValue>Number) {
+    resultArea.textContent = "돼지야 내려!";
+  } else if(userValue<Number) {
+    resultArea.textContent = "돼지야 올려!";
   } else {
-    ResultArea.textContent = "고생했어 우딩!!";
-    PlayBtn.disabled = true;
+    resultArea.textContent = "고생했어 우딩!!";
+    playBtn.disabled = true;
   }
-  History.push(UserInput.value)
+  History.push(userInput.value)
 
   if(Chances<1) {
-    GameOver = true;
+    gameOver = true;
   }
-  if(GameOver) {
-    PlayBtn.disabled = true;
-    ChanceArea.textContent = "멍청하네 돼지?"
-    ResultArea.textContent = "다시시작 해"
+  if(gameOver) {
+    playBtn.disabled = true;
+    chanceArea.textContent = "멍청하네 돼지?"
+    resultArea.textContent = "다시시작 해"
   }
 }
 
 function reset() {
-  UserInput.value = ""
-  RandomNumber()
-  ResultArea.textContent = "UP-Down!!"
+  userInput.value = ""
+  randomNumber()
+  resultArea.textContent = "UP-Down!!"
   Chances = 10;
-  ChanceArea.textContent = `남은 기회: ${Chances}`;
-  PlayBtn.disabled = false;
+  chanceArea.textContent = `남은 기회: ${Chances}`;
+  playBtn.disabled = false;
   History = [];
 }
 
-RandomNumber()
+randomNumber()
